9060f8156401290956399558c3969aee2db4208b
sagar-shrigadi <181492974+sagar-shrigadi@users.noreply.github.com>
2026-06-06 13:07:51 +0530

    feat(app): add cors and helmet middlewares

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 import { api } from "./Router/api.js";
 import { errHandler } from "./controller/errHandler.js";
 
 const app = express();
+
+app.use(cors());
+app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
