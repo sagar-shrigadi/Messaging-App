0ab17cb7bba9767bf73a39044c82f7b71a72acdd
sagar-shrigadi <181492974+sagar-shrigadi@users.noreply.github.com>
2026-06-09 14:12:17 +0530

    refactor(app): update cors origin for express and socket.io server from '*' to frontend domain

--- app.js
+++ app.js
@@ -14,10 +14,14 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://messaging-client.pages.dev",
   },
 });
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://messaging-client.pages.dev",
+  }),
+);
 app.use(helmet());
 
 app.use(express.json());
